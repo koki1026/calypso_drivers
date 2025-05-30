96a7278af81806b7df5272141d6dd78e38159bda
koki-22 <amakou2626@gmail.com>
2025-05-30 16:24:09 +0900

    dev: audio driver

---
+++ src/my_audio_node/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_audio_node'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='hira',
+    maintainer_email='akash.hira.c3@s.gifu-u.ac.jp',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+       entry_points={
+    'console_scripts': ['lake_test_node = my_audio_node.microphone_lake_testing:main',
+     'bag_to_wav_converter = my_audio_node.bag_to_wav_converter:main',
+    ],
+},
+)
