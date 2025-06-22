a256b60c36cd1ac7f28edb95573696d59dc6227c
Your Name <you@example.com>
2025-06-22 18:43:41 +0900

    dev: flow sensor

---
+++ src/flow_sensor/flow_sensor/flow_sensor_node.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32
+import Jetson.GPIO as GPIO
+import time
+import threading
+
+class FlowSensorNode(Node):
+    def __init__(self):
+        super().__init__('flow_sensor_node')
+
+        # GPIO設定
+        self.flow_pin = 7  # BOARDモードでのピン番号
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.flow_pin, GPIO.IN)
+
+        # パルスカウンタ
+        self.pulse_count = 0
+        GPIO.add_event_detect(self.flow_pin, GPIO.FALLING, callback=self.pulse_callback)
+
+        # ROSパブリッシャ
+        self.pub = self.create_publisher(Float32, 'flow_rate_lpm', 10)
+
+        # 1秒ごとにパルスを処理するタイマー
+        self.timer = self.create_timer(1.0, self.publish_flow_rate)
+
+        self.get_logger().info("FlowSensorNode started")
+
+    def pulse_callback(self, channel):
+        self.pulse_count += 1
+
+    def publish_flow_rate(self):
+        # 計算式: Q = pulse_count / 7.5 [L/min]
+        flow_rate = self.pulse_count / 7.5
+        msg = Float32()
+        msg.data = flow_rate
+        self.pub.publish(msg)
+        self.get_logger().info(f"Flow rate: {flow_rate:.2f} L/min")
+
+        # カウントをリセット
+        self.pulse_count = 0
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = FlowSensorNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        GPIO.cleanup()
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ src/flow_sensor/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'flow_sensor'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='calypso',
+    maintainer_email='you@example.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'flow_sensor_node = flow_sensor.flow_sensor_node:main',
+        ],
+    },
+)
